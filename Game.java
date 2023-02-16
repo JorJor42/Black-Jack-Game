ff94f1db6fa3c9931eccaa6c27fc2624928b3628
JorJor42 <125521783+JorJor42@users.noreply.github.com>
2023-02-15 20:57:30 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -2,6 +2,10 @@
  * SYST 17796 Project Base code.
  * Students can modify and extend to implement their game.
  * Add your name as an author and the date!
+  Jordan Vendrame
+ Fadil Nizar
+ Aromal Suresh Kumar
+ 15/02/2023
  */
 package ca.sheridancollege.project;
 
