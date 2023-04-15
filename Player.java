addb730b0fa3e28de644fb74f10b27332d71a31c
JorJor42 <125521783+JorJor42@users.noreply.github.com>
2023-04-15 17:37:56 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,17 +1,8 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- Jordan Vendrame
- Fadil Nizar
- Aromal Suresh Kumar
+/*
+ @author Jordan Vendrame
+ @author Fadil Nizar
+ @author Aromal Suresh Kumar
  12/04/2023
- */
-/**
- * A class that models each Player in the game. Players have an identifier, which should be unique.
- *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
  */
 
 public class Player
