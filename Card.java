70951b9719d75df6d7053f7323dd39a9b50c28e6
JorJor42 <125521783+JorJor42@users.noreply.github.com>
2023-04-15 17:36:20 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,18 +1,10 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- Jordan Vendrame
- Fadil Nizar
- Aromal Suresh Kumar
+/*
+ @author Jordan Vendrame
+ @author Fadil Nizar
+ @author Aromal Suresh Kumar
  12/04/2023
  */
-/**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
- * @author dancye
- */
+
 public class Card
 {
 
